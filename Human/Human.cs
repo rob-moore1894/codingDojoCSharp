17f01968d15475e1d17c38d8db5b1ced618d3b19
Rob Moore <moore.re@gmail.com>
2020-09-29 18:03:38 -0500

    Completed Human Project

---
+++ Human/Human.cs
@@ -0,0 +1,49 @@
+using System; 
+
+namespace Human{
+
+class Human {
+            public string Name; 
+            public int Strength; 
+            public int Intelligence; 
+            public int Dexterity;
+            private int health; 
+
+            // add a public "getter" property to access health
+            public int Health{
+                get {
+                    return health; 
+                }
+                set {
+                    health = value; 
+                }
+            }
+
+            // Add a constructor that takes a value to set Name, and set the remaining fields to default values
+            public Human(string name){
+                Name = name; 
+                Strength = 3; 
+                Intelligence = 3; 
+                Dexterity = 3; 
+                health = 100; 
+            }
+
+            // Add a constructor to assign custom values to all fields
+            public Human(string name, int strength, int intelligence, int dexterity, int health){
+                Name = name; 
+                Strength = strength;
+                Intelligence = intelligence; 
+                Dexterity = dexterity; 
+                Health = health; 
+            }
+
+            // Build Attack method
+            // Reduce the health of a Human object that is passed as a parameter. The damage done should be 5 * strength (5 points of damage to the attacked, for each 1 point of strength of the attacker). This method should return the remaining health of the target object.
+            public int Attack(Human target){
+                int damageDealt = this.Strength * 5; 
+                target.health = target.health - damageDealt;
+                Console.WriteLine($"{this.Name} attacked {target.Name} and reduced {target.Name}'s health by {damageDealt}.\n{this.Name} health = {this.Health}\n{target.Name} health = {target.Health}");  
+                return target.Health; 
+            }
+        }
+}
---
+++ Human/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        
+        static void Main(string[] args){
+            Human rob = new Human("Rob");
+            Console.WriteLine($"Name = {rob.Name}\nStrength = {rob.Strength}\nIntelligence = {rob.Intelligence}\nDexterity = {rob.Dexterity}\nHealth = {rob.Health}"); 
+
+            Console.WriteLine("====================================");
+            Human kenny = new Human("Kenny", 5, 7, 8, 100); 
+            Console.WriteLine($"Name = {kenny.Name}\nStrength = {kenny.Strength}\nIntelligence = {kenny.Intelligence}\nDexterity = {kenny.Dexterity}\nHealth = {kenny.Health}");
+
+            rob.Attack(kenny); 
+        }
+    }
+}
