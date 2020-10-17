7fd8b885bd70528ffbb7243efefc069494893161
Rob Moore <moore.re@gmail.com>
2020-10-17 15:10:49 -0500

    Completed Random Passcode

---
+++ RandomPasscode/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RandomPasscode.Models;
+
+namespace RandomPasscode.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        [HttpPost("generate")]
+        public IActionResult Generate()
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            int length = 14;
+            string randomStr = "";
+            var random = new Random(); 
+            for (int i = 0; i < length; i++){
+                int a = random.Next(chars.Length);
+                randomStr += chars.ElementAt(a);
+            }
+            ViewBag.randomStr = randomStr; 
+            Console.WriteLine(randomStr);
+            return RedirectToAction("Index", randomStr); 
+        }
+
+    }
+}
---
+++ RandomPasscode/Models/Passcode.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;  
+
+namespace RandomPasscode
+{
+    public class RandCode
+    {
+        public string Passcode {get; set;}
+    }
+}
