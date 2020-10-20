f2e338105eedddfbea5d4133822c591fdf536003
Rob Moore <moore.re@gmail.com>
2020-10-20 11:57:42 -0500

    Completed LoginRegistration

---
+++ LoginRegister/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LoginRegister.Models;
+using Microsoft.AspNetCore.Identity; 
+using Microsoft.AspNetCore.Http;
+
+namespace LoginRegister.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext _context;
+    
+        // here we can "inject" our context service into the constructorcopy
+        public HomeController(MyContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            HttpContext.Session.Clear(); 
+            return View();
+        }
+
+        [HttpGet("Logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear(); 
+            return View("Index");
+        }
+
+        [HttpPost("Register")]
+        public IActionResult Register(User newUser)
+        {
+            if(ModelState.IsValid)
+            {
+                if(_context.Users.Any(e => e.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "Email is already in use");
+                    return View("Index");
+                }
+                PasswordHasher<User> Hasher = new PasswordHasher<User>(); 
+                newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+                _context.Add(newUser);
+                _context.SaveChanges();
+                HttpContext.Session.SetInt32("logged_in", newUser.UserId);
+                return RedirectToAction("Success");
+            } else {
+                return View("Index");
+            }
+        }
+        [HttpPost("Login")]
+        public IActionResult Login(LUser login)
+        {
+            if(ModelState.IsValid)
+            {
+                User userInDb = _context.Users.FirstOrDefault(u => u.Email == login.LEmail); 
+                if(userInDb == null)
+                {
+                    ModelState.AddModelError("LEmail", "Invalid Login"); 
+                    return View("Index");
+                } else {
+                    var hasher = new PasswordHasher<LUser>();
+                    var result = hasher.VerifyHashedPassword(login, userInDb.Password, login.LPassword);
+                    if(result == 0)
+                    {
+                        ModelState.AddModelError("LEmail", "Invalid Login"); 
+                        return View("Index");
+                    }
+                    HttpContext.Session.SetInt32("logged_in", userInDb.UserId);
+                    return RedirectToAction("Success");
+                }
+            } else {
+                return View("Index"); 
+            }
+        }
+
+        [HttpGet("Success")]
+        public IActionResult Success()
+        {
+            int? loggedIn = HttpContext.Session.GetInt32("logged_in");
+            if(loggedIn != null)
+            {
+                ViewBag.User = _context.Users.FirstOrDefault(a => a.UserId == (int)loggedIn);
+                return View();
+            } else {
+                return RedirectToAction("Index");
+            }
+        }
+        
+    }
+}
---
+++ LoginRegister/Models/User.cs
@@ -0,0 +1,41 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoginRegister.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId {get; set;}
+
+        [Required]
+        public string FirstName {get; set;}
+
+        [Required]
+        public string LastName {get; set;}
+
+        [Required]
+        [EmailAddress]
+        public string Email {get; set;}
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DoB {get; set;}
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8)]
+        public string Password {get; set;}
+
+        public DateTime CreatedAt {get; set;} = DateTime.Now;
+
+        public DateTime UpdatedAt {get; set;} = DateTime.Now;
+
+        [NotMapped]
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string Confirm {get; set;}
+    }
+}
